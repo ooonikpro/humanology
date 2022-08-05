712c3b1fe732677ca6d2a975fe1b2e9431f37c00
Oolah <konight@gmail.com>
2022-08-05 18:38:16 +0300

    Header. WIP.

--- src/components/AppContent/AppContent.tsx
+++ src/components/AppContent/AppContent.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Icon } from '../Icon'
+import { Illustration } from '../IllustrationQuadraCircle/IllustrationQuadraCircle'
 import { Line } from '../Line'
 
 
@@ -11,6 +12,7 @@ export const AppContent = () => {
 			<br /> 
 			<Icon name='Aspect01Business' size={64} color={'accent'} />
 			<Icon name='Aspect02Logic' size={64} color={'black'} />
+			<Illustration name='QuadraCircle02FireEthic' size={128} color={'black'} />
 		</div>
 		
 	)
--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -2,14 +2,26 @@ import React from 'react'
 
 import { Icon } from '../Icon';
 import styles from './AppHeader.module.scss';
+import { Line } from '../Line';
 
 export const AppHeader = () => {
 	return (
 		<header className={styles.header}>
-			<div className='{styles.section}'>
-				<Icon name='IconMenu' size={24} color='black'/>
-				<Icon name='Logo' size={24} color='black'/>
-			</div>
-		</header>
+			<div className={styles.section}>
+				<button 
+				className={`${styles.btn} ${styles['btn-o50']}`}>
+					<Icon name='IconMenu' size={24} />
+				</ button>
+				<button 
+					className={`${styles.btn} ${styles['btn-025']}`}>
+					<Icon name='Logo' size={24} color='role' />
+				</ button>
+				<button
+				className={`${styles.btn} ${styles['btn-o50']}`}>
+					<Icon name='IconUserSquare' size={24} />
+				</button>
+			</div>		
+			<Line solid color='accent' opacity='o10' />
+		</ header>
 	)
 }
