53d73faf6ea822d42cbe485e5344bba9203764e2
Никита Овечкин <pumpingmonster04b63@gmail.com>
2022-08-29 16:59:43 +0200

    chore: rename mindSets

--- src/containers/MindSets/Mindsets.tsx
+++ src/containers/MindSets/Mindsets.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import styles from './Mindsets.module.scss';
+import styles from './MindSets.module.scss';
 import { WhiteCard } from '../../components/WhiteCard';
 import { Text } from '../../components/Text';
 import { AuthorLine } from '../../components/AuthorLine';
 
-export default function Mindsets() {
+export default function MindSets() {
     return (
         <div className={styles.root}>
             <WhiteCard color="beige-title-big">
                 <Text tag="h1" size="h4" font="additional" className={styles.title}>Типы мышления</Text>
-                <AuthorLine readTime='5'>äлеша</AuthorLine> 
+                <AuthorLine readTime='5'>äлеша</AuthorLine>
 
                 <Text tag="p" className={styles.text}>
                     Разные стили мышления, соответствующие тем или иным типам личности определяют особенности интеллекта, а значит и склонность к тем или иным видам деятельности.
