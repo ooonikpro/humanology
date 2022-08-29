913983f3531fd45b2c8c57891b6335585374a0db
Никита Овечкин <pumpingmonster04b63@gmail.com>
2022-08-29 18:14:34 +0200

    fix: correct modal behavior

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { EndPoint } from '../EndPoint';
 import { WhiteCard } from '../WhiteCard';
 import styles from './Modal.module.scss';
+
+let openedModals: Array<HTMLElement> = [];
+
+const ANIMATE_DURATION = 180;
+const target = document.getElementById('modals') as HTMLElement;
 
 export interface ModalProps {
     isOpen: boolean;
@@ -13,29 +19,34 @@ interface Props extends ModalProps {
 }
 
 export const Modal: React.FC<Props> = ({ isOpen, render, onClose }) => {
+    const container = useMemo(() => document.createElement('div'), []);
     const [animate, setAnimate] = useState(false);
     const [isShow, setIsShow] = useState(false);
     const showModal = () => {
         setIsShow(true);
         setAnimate(true);
+        openedModals.push(container);
     };
     const hideModal = () => {
         setAnimate(false);
-        setTimeout(() => setIsShow(false), 250);
+        setTimeout(() => setIsShow(false), ANIMATE_DURATION);
     };
     const closeModal = () => {
         setAnimate(false);
 
-        setTimeout(onClose, 250);
+        setTimeout(() => {
+            onClose();
+            openedModals = openedModals.filter((c) => c != container);
+        }, ANIMATE_DURATION);
     };
 
-    const onEscPress = (e: KeyboardEvent) => {
-        if (e.key === 'Escape') {
-            hideModal();
+    const onEscPress = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape' && openedModals.at(-1) === container) {
+            closeModal();
 
             document.removeEventListener('keydown', onEscPress);
         }
-    };
+    }, []);
 
     const onMount = () => {
         document.documentElement.classList.add('no-scroll');
@@ -43,7 +54,10 @@ export const Modal: React.FC<Props> = ({ isOpen, render, onClose }) => {
     };
 
     const onUnmount = () => {
-        document.documentElement.classList.remove('no-scroll');
+        if (openedModals.length === 0) {
+            document.documentElement.classList.remove('no-scroll');
+        }
+
         document.removeEventListener('keydown', onEscPress);
     };
 
@@ -63,14 +77,19 @@ export const Modal: React.FC<Props> = ({ isOpen, render, onClose }) => {
         return null;
     }
 
-    return (
-        <div className={[styles.root, animate ? styles.open : ''].join(' ')}>
-            <div className={styles.bg} onClick={closeModal} />
-            <WhiteCard color="beige" className={styles.container}>
-                <div className={styles.drag} onClick={closeModal} />
-                {render(closeModal)}
-                <EndPoint className={styles.endpoint}/>
-            </WhiteCard>    
-        </div>
+    return createPortal(
+        (
+            <div className={[styles.root, animate ? styles.open : ''].join(' ')}>
+                <div className={styles.bg} onClick={closeModal} />
+                <div className={styles.inner}>
+                    <WhiteCard color="beige" className={styles.container}>
+                        <div className={styles.drag} onClick={closeModal} />
+                        {render(closeModal)}
+                        <EndPoint className={styles.endpoint}/>
+                    </WhiteCard>
+                </div>
+            </div>
+        ),
+        target
     );
 };
