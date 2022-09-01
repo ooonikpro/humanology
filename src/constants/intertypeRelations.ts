fbd4e7463bc27f8142f3dfccb074c325d016cf9f
alesha <konight@gmail.com>
2022-09-01 21:41:08 +0300

    Added intertype icons.

--- src/constants/intertypeRelations.ts
+++ src/constants/intertypeRelations.ts
@@ -1,85 +1,85 @@
-import { Socionics } from "../types";
+import { Socionics } from '../types';
 
 export const INTERTYPE_RELATIONS : Record<Socionics.intertypeRelations, {name: string, lvl: number}> = {
 
-	dual: {
-		name: 'Дуальные',
-		lvl: 4
-	},
+    dual: {
+        name: 'Дуальные',
+        lvl: 4
+    },
 
-	activity: {
-		name: 'Активации',
-		lvl: 3
-	},
+    activity: {
+        name: 'Активации',
+        lvl: 3
+    },
 
-	mirror: {
-		name: 'Зеркальные',
-		lvl: 2
-	},
+    mirror: {
+        name: 'Зеркальные',
+        lvl: 2
+    },
 
-	identical: {
-		name: 'Тождественные',
-		lvl: 2
-	},
+    identical: {
+        name: 'Тождественные',
+        lvl: 2
+    },
 
-	kindred: {
-		name: 'Родственные',
-		lvl: 2
-	},
+    kindred: {
+        name: 'Родственные',
+        lvl: 2
+    },
 
-	semidual: {
-		name: 'Полудуальные',
-		lvl: 3
-	},
+    semidual: {
+        name: 'Полудуальные',
+        lvl: 3
+    },
 
-	pragmatic: {
-		name: 'Деловые',
-		lvl: 2
-	},
+    pragmatic: {
+        name: 'Деловые',
+        lvl: 2
+    },
 
-	illusionary: {
-		name: 'Миражные',
-		lvl: 3
-	},
+    illusionary: {
+        name: 'Миражные',
+        lvl: 3
+    },
 
-	beneficiary: {
-		name: 'Заказа',
-		lvl: 2
-	},
+    beneficiary: {
+        name: 'Заказа',
+        lvl: 2
+    },
 
-	giver: {
-		name: 'Подзаказный',
-		lvl: 1
-	},
+    giver: {
+        name: 'Подзаказный',
+        lvl: 1
+    },
 
-	supervisor: {
-		name: 'Ревизор',
-		lvl: 2
-	},
+    supervisor: {
+        name: 'Ревизор',
+        lvl: 2
+    },
 
-	underling: {
-		name: 'Подревизный',
-		lvl: 2
-	},
+    underling: {
+        name: 'Подревизный',
+        lvl: 2
+    },
 
-	quasidentical: {
-		name: 'Квазитождественные',
-		lvl: 2
-	},
+    quasidentical: {
+        name: 'Квазитождественные',
+        lvl: 2
+    },
 
-	parallel: {
-		name: 'Паралелльные',
-		lvl: 2
-	},
+    parallel: {
+        name: 'Паралелльные',
+        lvl: 2
+    },
 
-	contrary: {
-		name: 'Погашения',
-		lvl: 1
-	},
+    contrary: {
+        name: 'Погашения',
+        lvl: 1
+    },
 
-	superego: {
-		name: 'Суперэго',
-		lvl: 1
-	},
+    superego: {
+        name: 'Суперэго',
+        lvl: 1
+    },
 
-}
+};
