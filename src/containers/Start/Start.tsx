ac3171799b64acb7e1c1d5520b9c5853a1f008c8
alesha <me@alesha.io>
2022-09-09 20:00:47 +0300

    Test FTP Deploy 5

--- src/containers/Start/Start.tsx
+++ src/containers/Start/Start.tsx
@@ -11,7 +11,7 @@ export default function Menu() {
                 <Text tag="h1" size="h4" font="additional" className={styles.title}>Начало</Text>
                 <AuthorLine text="Читать меньше минуты" iconName="Time">äлеша</AuthorLine>
                 <Text tag="p" size="base" font="base">Это пока Альфаверсия. <br />Могут присутствовать бесячие баги и зависания. <br />Пока просто перезагружайте страницу.</Text>
-                <Text tag="p" size="base" font="base">Тут будет ликбез по соционике и всякие объяснялки для начинающих постигать истину.</Text>
+                <Text tag="p" size="base" font="base">Тут будет ликбез по соционике и всякие статьи для начинающих постигать истину.</Text>
                 <hr />
                 <Text tag="p" size="base" font="base">А пока читайте <a href="https://teletype.in/@humanology">девблог на Телетайпе</a>.</Text>
             </WhiteCard>
