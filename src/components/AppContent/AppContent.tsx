2a934621adcff5f85afe0ed20e8b2c1b8a6a34d0
Никита Овечкин <n.ovechkin.ccs@gmail.com>
2022-10-05 22:11:15 +0200

    fix: scroll

--- src/components/AppContent/AppContent.tsx
+++ src/components/AppContent/AppContent.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from './AppContent.module.scss';
 
+const appContent = document.querySelector('.app-content');
+
+
 export const AppContent: React.FC<{ children: React.ReactNode | React.ReactNode[] }> = ({ children }) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (appContent) {
+            appContent.scrollTop = 0;
+        }
+    }, [location]);
+
     return (
         <main className={styles.root}>
             { children }
