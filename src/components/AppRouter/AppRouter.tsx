ae675ecb6b1f7d5aa99bde6d9a6f6810cc048b14
Nikita Ovechkin <pumpingmonster04b63@gmail.com>
2022-10-16 08:36:54 +0200

    feat: async routes part 1

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route as RouteType, routes } from '../../routes';
 import { Route, Routes } from 'react-router-dom';
 
 const createRoute = (route: RouteType, key: number) => {
-    const Container = route.container;
+    const Container = React.lazy(route.container);
+    const element = <Suspense><Container/></Suspense>;
 
     if (route.children && Array.isArray(route.children)) {
         return (
-            <Route key={key} path={route.path} index={route.index} element={<Container/>}>
+            <Route key={key} path={route.path} index={route.index} element={element}>
                 { route.children.map(createRoute) }
             </Route>
         );
     }
 
     return (
-        <Route key={key} path={route.path} element={<Container/>}/>
+        <Route key={key} path={route.path} element={element}/>
     );
 };
 
