5b4b2e49a6e62b48a9815862c75d6689bbb60907
Никита Овечкин <pumpingmonster04b63@gmail.com>
2023-02-02 17:43:23 +0200

    commit

--- src/components/CardGroupByElement/CardGroupByElement.tsx
+++ src/components/CardGroupByElement/CardGroupByElement.tsx
@@ -4,6 +4,9 @@ import { useColorElement } from '../../hooks';
 import { Socionics } from '../../types';
 import { Text } from '../Text';
 import { Icon, getIconName } from '../Icon';
+import { NATURE_LINKS } from 'src/constants/children';
+import { Link } from 'react-router-dom';
+import { ROUTES } from 'src/constants/routes';
 
 interface Props {
     element: Socionics.Quadras;
@@ -13,12 +16,23 @@ interface Props {
 export const CardGroupByElement: React.FC<Props> = ({ element, children }) => {
     const { label, color } = useColorElement(element);
 
+    const texts = NATURE_LINKS[element];
+    const link = ROUTES.QUADRAS_ITEM(element);
+
     return (
         <div className={styles.root} style={color}>
             <div className={styles.header}>
                 <Icon className={styles.quadraCircle} name={getIconName(element.concat('Mini'))} color="element" size={64} />
                 <Text tag="p" size="h4" font="additional" color="element" className={styles['header-title']}>{label}</Text>
-                <Icon name="Help" size={24} className={styles['header-icon']} />
+
+                <Link to={link} className={styles.header_link}>
+                    {
+                        texts.map((item, $i) => (
+                            <Text color="accent" key={$i}>{item}</Text>
+                        ))
+                    }
+                    <Icon name="Help" size={24} className={styles['header-icon']} />
+                </Link>
             </div>
 
             <div className={styles.body}>
---
+++ src/constants/children.ts
@@ -0,0 +1,8 @@
+import { Socionics } from 'src/types';
+
+export const NATURE_LINKS: Record<Socionics.Quadras, string[]> = {
+    air: ['Дети', 'Альфа'],
+    fire: ['Дети', 'Альфа'],
+    water: ['Дети', 'Альфа'],
+    earth: ['Дети', 'Альфа'],
+};
